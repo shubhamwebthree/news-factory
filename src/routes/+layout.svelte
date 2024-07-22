0e9ae23ced143e67cc70299f23a338d2a4416076
https://github.com/ShubhamWeb3 <shubham0712kumar@gmail.com>
2024-07-22 20:14:28 +0530

    first

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,11 @@
+<script>
+    import Dashboard from '$lib/Dashboard.svelte';
+import Navbar from '$lib/Navbar.svelte'
+// import Footer from '$lib/Footer.svelte'
+import "../app.css"
+</script>
+
+<Navbar />
+<!-- <slot /> -->
+<Dashboard/>
+<!-- <Footer /> -->
