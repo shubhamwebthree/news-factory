a20083877f90df0f07e6a42e0e5de9e06788c657
https://github.com/ShubhamWeb3 <shubham0712kumar@gmail.com>
2024-07-23 13:07:47 +0530

    connect-wallet

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,11 +1,11 @@
 <script>
-    import Dashboard from '$lib/Dashboard.svelte';
+import Dashboard from '$lib/Dashboard.svelte';
 import Navbar from '$lib/Navbar.svelte'
 // import Footer from '$lib/Footer.svelte'
 import "../app.css"
 </script>
 
 <Navbar />
-<!-- <slot /> -->
-<Dashboard/>
+<slot />
+<!-- <Dashboard/> -->
 <!-- <Footer /> -->
